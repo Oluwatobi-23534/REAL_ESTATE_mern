7681689429c837038e1a676c654034f7bdd26ad8
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-11-05 09:35:15 +0100

    created user update api

--- api/routes/user_route.js
+++ api/routes/user_route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { route } from '../controllers/user_controller.js';
+import { route, updateUser } from '../controllers/user_controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/route', route);
+router.post('/update/:id', verifyToken, updateUser);
 
 export default router;

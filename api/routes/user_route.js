eb51c404cf960fe9d6d2081b3d43ed202ed4c76c
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-11-05 11:24:10 +0100

    complete delete user functionality

--- api/routes/user_route.js
+++ api/routes/user_route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { route, updateUser } from '../controllers/user_controller.js';
+import { deleteUser, route, updateUser } from '../controllers/user_controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/route', route);
 router.post('/update/:id', verifyToken, updateUser);
+router.delete('/delete/:id', verifyToken, deleteUser);
 
 export default router;

8855a06295d67058a6567b153fddda263ba27eda
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-11-07 16:15:25 +0100

    created get user listings api routes

--- api/controllers/user_controller.js
+++ api/controllers/user_controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user_model.js";
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing_model.js";
 
 export const route = (req, res) => {
   res.json({
@@ -31,7 +32,8 @@ export const updateUser = async (req, res, next) => {
 
     const { password, ...rest } = updatedUser._doc;
 
-    res.status(200).json(rest);
+    re
+s.status(200).json(rest);
   } catch (error) {
     next(error);
   }
@@ -46,5 +48,19 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+
+  if(req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };

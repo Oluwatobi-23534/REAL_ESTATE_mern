259f8f49f73493757c340d8d75a87c2fe30035a6
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-11-10 00:16:15 +0100

    Made minor changes to the contact page

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -4,42 +4,43 @@ import { Link } from 'react-router-dom';
 export default function Contact({ listing }) {
   const [landlord, setLandlord] = useState(null);
   const [message, setMessage] = useState("");
-  const onChange = (e) => {
-    setMessage(e.target.value);
+
+  const handleChange = (event) => {
+    setMessage(event.target.value);
   };
 
   useEffect(() => {
-    const fetchLandlord = async () => {
+    const fetchLandlordDetails = async () => {
       try {
-        const res = await fetch(`/api/user/${listing.userRef}`);
-        const data = await res.json();
-        setLandlord(data);
+        const response = await fetch(`/api/user/${listing.userRef}`);
+        const landlordData = await response.json();
+        setLandlord(landlordData);
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
     };
-    fetchLandlord();
+    fetchLandlordDetails();
   }, [listing.userRef]);
+
   return (
     <>
       {landlord && (
         <div className="flex flex-col gap-2">
           <p>
-            Contact <span className="font-semibold">{landlord.username}</span>{" "}
-            for{" "}
-            <span className="font-semibold">{listing.name.toLowerCase()}</span>
+            Reach out to <span className="font-semibold">{landlord.username}</span> 
+            regarding <span className="font-semibold">{listing.name.toLowerCase()}</span>
           </p>
           <textarea
             name="message"
             id="message"
             rows="2"
             value={message}
-            onChange={onChange}
+            onChange={handleChange}
             placeholder="Enter your message here..."
             className="w-full border p-3 rounded-lg"
           ></textarea>
           <Link
-            to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+            to={`mailto:${landlord.email}?subject=Inquiry about ${listing.name}&body=${message}`}
             className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
           >
             Send Message

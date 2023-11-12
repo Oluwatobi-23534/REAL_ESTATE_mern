56103ad6a516785379232648680ac9f6d21e0fe1
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-11-12 16:27:33 +0100

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,14 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>Introducing Big-City Estate</h1>
+      <p className='mb-4 text-slate-700'>Big-City Estate is a distinguished real estate agency that excels in facilitating the acquisition, sale, and leasing of properties in the most sought-after locales. Our cadre of seasoned agents is committed to delivering unparalleled service, ensuring a seamless and rewarding property transaction experience.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is to empower our clients to realize their real estate aspirations through expert counsel, bespoke service, and an intimate understanding of the local market dynamics. Whether you&apos;re in the market to purchase, sell, or lease a property, we stand ready to guide you through every stage of the journey.
+      </p>
+      <p className='mb-4 text-slate-700'>Our team of agents brings a wealth of industry experience and knowledge to the table, and we are steadfast in our commitment to delivering the pinnacle of service to our clientele. We believe that property transactions should be both exhilarating and rewarding, and we are devoted to transforming this belief into a reality for each of our esteemed clients.</p>
+    </div>
   )
 }
